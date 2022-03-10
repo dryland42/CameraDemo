37c113f57322bf1d735fc3649763e8fdf58170f9
dryland42 <rylan@rmrdevelopment.com>
2022-03-10 13:27:57 -0500

    first commit

---
+++ CameraDemo/CameraDemo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace CameraDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Uri uri = new Uri("https://fir-rtc-4522a.firebaseapp.com/");
+            OpenBrowser(uri);
+        }
+
+        public async Task OpenBrowser(Uri uri)
+        {
+            try
+            {
+                await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                // An unexpected error occured. No browser may be installed on the device.
+            }
+        }
+    }
+}
